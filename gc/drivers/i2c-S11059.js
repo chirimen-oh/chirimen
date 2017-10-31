d917805f5ec7c8ffe8fbdd785d8a1af91ecbbcf1
tadfmac <dfm@ta-music.net>
2017-10-31 16:26:40 +0900

    #3 fix S11059 colors

--- gc/drivers/i2c-S11059.js
+++ gc/drivers/i2c-S11059.js
@@ -22,28 +22,76 @@ S11059.prototype = {
     });
   },
   compose: function(high, low) {
-    var val = ((high << 8) + low) >> 4;
+    var val = ((high << 8) + low);
     return val;
   },
+
+  // read Real RGB+Ir data
   read: function() {
     return new Promise(async (resolve, reject)=>{
       if(this.i2cSlave == null){
         reject("i2cSlave Address does'nt yet open!");
       }else{
-        await this.i2cSlave.write8(0x00, 0x89);
-        await this.i2cSlave.write8(0x00, 0x09);
-        await this.sleep(10);
-        await this.i2cSlave.write8(0x00, 0x0A);
+        await this.i2cSlave.write8(0x00, 0x8a);
+        await this.i2cSlave.write8(0x00, 0x0a);
         await this.sleep(100);
 
         await this.i2cSlave.writeByte(0x03);
-        this.i2cSlave.readBytes(6).then((v)=>{
+        this.i2cSlave.readBytes(8).then((v)=>{
 //          console.log("RH:"+v[0]+" RL:"+v[1]+" GH:"+v[2]+" GL:"+v[3]+" BH:"+v[4]+" GL:"+v[5]);
           var red = this.compose(v[0], v[1]);
           var green = this.compose(v[2], v[3]);
           var blue = this.compose(v[4], v[5]);
+          var ir = this.compose(v[6], v[7]);
 //          console.log("read ok!");
-          resolve([red, green, blue]);
+          resolve([red, green, blue,ir]);
+
+        },(err)=>{
+          reject(err);
+
+        });
+
+      }
+    });
+  },
+
+  // read Normalization RGB 8bit data with auto-gain)
+  readR8G8B8: function() {
+    return new Promise(async (resolve, reject)=>{
+      if(this.i2cSlave == null){
+        reject("i2cSlave Address does'nt yet open!");
+      }else{
+        await this.i2cSlave.write8(0x00, 0x8a);
+        await this.i2cSlave.write8(0x00, 0x0a);
+        await this.sleep(100);
+
+        await this.i2cSlave.writeByte(0x03);
+        this.i2cSlave.readBytes(8).then((v)=>{
+//          console.log("RH:"+v[0]+" RL:"+v[1]+" GH:"+v[2]+" GL:"+v[3]+" BH:"+v[4]+" GL:"+v[5]);
+          var red_org = this.compose(v[0], v[1]);
+          var green_org = this.compose(v[2], v[3] * 1.1);
+          var blue_org = this.compose(v[4], v[5] * 2.6);
+          var gain_level = 0;
+
+         // Auto-gain
+          if ((red_org > green_org) && (red_org > blue_org)) {
+            gain_level = (red_org /255);
+          }
+          if ((green_org > red_org) && (green_org > blue_org)) {
+            gain_level = (green_org /255);
+          }
+          if ((blue_org > green_org) && (blue_org > red_org)) {
+            gain_level = (blue_org /255);
+          }
+          if (gain_level < 1) {
+             gain_level = 1;
+          }
+          var red8= (red_org / gain_level) & 0xff;
+          var green8 = (green_org / gain_level) & 0xff;
+          var blue8 = (blue_org / gain_level) & 0xff;
+
+//          console.log("read ok!");
+          resolve([red8, green8, blue8,gain_level]);
 
         },(err)=>{
           reject(err);
--- gc/i2c/i2c-S11059/main.js
+++ gc/i2c/i2c-S11059/main.js
@@ -7,15 +7,16 @@ window.addEventListener('load', function (){
     var s11059 = new S11059(port,0x2a);
     s11059.init().then(()=>{
       setInterval(()=>{
-        s11059.read().then((values)=>{
-          var red = values[0];
-          var green = values[1];
-          var blue = values[2];
+
+        s11059.readR8G8B8().then((values)=>{
+          var red = values[0] & 0xff;
+          var green = values[1] & 0xff;
+          var blue = values[2] & 0xff;
+          var gain_level = values[3];
+
 //          console.log("red:" + red + " green:" + green + " blue:" + blue);
-          document.getElementById("red").textContent = "R:"+red;
-          document.getElementById("green").textContent = "G:"+green;
-          document.getElementById("blue").textContent = "B:"+blue;
-          document.getElementById("color").style.backgroundColor = "rgb("+red+","+green+","+blue+")"; 
+          document.getElementById("sensor").textContent = "R:"+red+" G:"+green+" B:"+blue+" GAIN:"+gain_level;
+          document.getElementById("color").style.backgroundColor = "rgb("+red+", "+green+","+blue+")";
         }).catch(function(reason) {
           console.log("READ ERROR:" + reason);
         });
@@ -26,5 +27,5 @@ window.addEventListener('load', function (){
   },(err)=>{
     console.log("requestI2CAccess error");
   });
-    
+
 }, false);
