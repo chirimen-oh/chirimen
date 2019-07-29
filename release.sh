eefb0606021e0b52a47a691f257a0e778310e211
dynamis <dynamis@firehacks.org>
2019-07-29 18:00:54 +0900

    Update release.sh

--- release.sh
+++ release.sh
@@ -9,19 +9,25 @@ PUBLISH_DIR=${BASE_DIR}/public
 rm -rf ${PUBLISH_DIR}
 mkdir -p ${PUBLISH_DIR}
 
-rm -f ./_gc/version.txt
-rm -f ./gc/version.txt
+rm -f ${BASE_DIR}/_gc/version.txt
+rm -f ${BASE_DIR}/gc/version.txt
 date +"CHIRIMEN for Raspberry Pi 3 env.version : %y/%m/%d:%H:%M:%S" > ${PUBLISH_DIR}/version.txt
-cp ${PUBLISH_DIR}/version.txt ./_gc/version.txt
-cp ${PUBLISH_DIR}/version.txt ./gc/version.txt
+cp ${PUBLISH_DIR}/version.txt ${BASE_DIR}/_gc/version.txt
+cp ${PUBLISH_DIR}/version.txt ${BASE_DIR}/gc/version.txt
 
-zip -r ${PUBLISH_DIR}/gc.zip ./gc/
-zip -r ${PUBLISH_DIR}/_gc.zip ./_gc/
+zip -r ${PUBLISH_DIR}/gc.zip ${BASE_DIR}/gc/
+zip -r ${PUBLISH_DIR}/_gc.zip ${BASE_DIR}/_gc/
 
 zip --delete ${PUBLISH_DIR}/gc.zip "*__MACOSX*"
 zip --delete ${PUBLISH_DIR}/_gc.zip "*__MACOSX*"
 zip --delete ${PUBLISH_DIR}/gc.zip "*.DS_Store"
 zip --delete ${PUBLISH_DIR}/_gc.zip "*.DS_Store"
+
+echo "result log:"
+echo "BASE_DIR = ${BASE_DIR}"
+ls ${BASE_DIR}
+echo "PUBLISH_DIR = ${PUBLISH_DIR}"
+ls ${PUBLISH_DIR}
 
 # polyfill/drivers should be handled by other deployment
 #rm -rf ./release/cdn
