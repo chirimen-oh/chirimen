ca21d088683c7dec8d81bd9a1bd37234a75f1366
dynamis <dynamis@firehacks.org>
2019-07-31 23:09:44 +0900

    prettier and use getElementById, not querySelector

--- gc/testSet/i2c-ADT7410_49/main.js
+++ gc/testSet/i2c-ADT7410_49/main.js
@@ -1,17 +1,23 @@
-'use strict';
+"use strict";
 
-window.addEventListener('load', function (){
-  var head = document.querySelector('#head');
-  navigator.requestI2CAccess().then((i2cAccess)=>{
-    var port = i2cAccess.ports.get(1);
-    var adt7410 = new ADT7410(port,0x49);
-    adt7410.init().then(()=>{
-      setInterval(()=>{
-        adt7410.read().then((value)=>{
-//          console.log('value:', value);
-          head.innerHTML = value ? value : head.innerHTML;
-        });
-      },1000);
-    });
-  }).catch(e=> console.error('error', e));
-}, false);
+window.addEventListener("load", mainFunction, false);
+
+async function mainFunction() {
+  var head = document.getElementById("head");
+
+  navigator
+    .requestI2CAccess()
+    .then(i2cAccess => {
+      var port = i2cAccess.ports.get(1);
+      var adt7410 = new ADT7410(port, 0x49);
+      adt7410.init().then(() => {
+        setInterval(() => {
+          adt7410.read().then(value => {
+            //          console.log('value:', value);
+            head.innerHTML = value ? value : head.innerHTML;
+          });
+        }, 1000);
+      });
+    })
+    .catch(e => console.error("error", e));
+}
