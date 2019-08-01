1958c0006c46d99085754b1b52d9da69f88df098
dynamis <dynamis@firehacks.org>
2019-08-01 15:46:27 +0900

    add comment for another slave address

--- gc/contrib/examples/i2c-BME280/main.js
+++ gc/contrib/examples/i2c-BME280/main.js
@@ -7,6 +7,8 @@ async function main() {
     var humi = document.getElementById("humi");
     var i2cAccess = await navigator.requestI2CAccess();
     var port = i2cAccess.ports.get(1);
+    // BME280 の SlaveAddress 初期値はモジュールによって異なる
+    // var bme280 = new BME280(port, 0x76);
     var bme280 = new BME280(port, 0x77);
     await bme280.init();
     while (1) {
