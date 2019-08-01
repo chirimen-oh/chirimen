a4db652f43c55a4b47bf6591fd8e8064599dab62
Kohei Watanabe <kou029w@gmail.com>
2019-08-01 16:19:27 +0900

    fix example code

--- gc/gpio/button/main.js
+++ gc/gpio/button/main.js
@@ -1,11 +1,13 @@
 main();
 
-function main() {
-  var onoff = document.getElementById("onoff");
-  var ledView = document.getElementById("ledView");
-  var v = 0;
-  onoff.onclick = function controlLed() {
-    v = v === 0 ? 1 : 0;
-    ledView.style.backgroundColor = v === 1 ? "red" : "black";
+async function main() {
+  var gpioAccess = await navigator.requestGPIOAccess();
+  var ledPort = gpioAccess.ports.get(26); // LEDの付いているポート
+  await ledPort.export("out");
+  var switchPort = gpioAccess.ports.get(5); // タクトスイッチの付いているポート
+  await switchPort.export("in");
+  switchPort.onchange = function(val) {
+    // スイッチはPullupで離すと1なので反転させる
+    ledPort.write(val === 0 ? 1 : 0);
   };
 }
