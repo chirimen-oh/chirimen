76337b1723072a1fa9bb96c6d3ffe07513b8bcb7
Satoru Takagi <satakagi@gmail.com>
2019-10-12 14:27:46 +0900

    Add SSD1306 support to oledDisplay driver

---
+++ gc/i2c/i2c-grove-oledDisplay/main_1306.js
@@ -0,0 +1,19 @@
+main();
+
+async function main() {
+  var head = document.getElementById("head");
+  head.innerHTML = "started";
+  var i2cAccess = await navigator.requestI2CAccess();
+  head.innerHTML = "initializing...";
+  var port = i2cAccess.ports.get(1);
+  var display = new OledDisplay(port,0x3c); // set Address to 0x3C
+  await display.init(true); // use SSD1306
+  display.clearDisplayQ();
+  await display.playSequence();
+  head.innerHTML = "drawing text...";
+  display.drawStringQ(0, 0, "hello");
+  display.drawStringQ(1, 0, "Real");
+  display.drawStringQ(2, 0, "World");
+  await display.playSequence();
+  head.innerHTML = "completed";
+}
