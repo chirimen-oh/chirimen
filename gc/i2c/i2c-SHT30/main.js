9440bdea36f0b6c17d3eca802450fe4a26822c01
Kohei Watanabe <kou029w@gmail.com>
2020-11-18 15:07:53 +0900

    use jsdelivr

--- gc/i2c/i2c-SHT30/main.js
+++ gc/i2c/i2c-SHT30/main.js
@@ -1,19 +1,17 @@
 main();
 
 async function main() {
-  try {
-    var i2cAccess = await navigator.requestI2CAccess();
-    var port = i2cAccess.ports.get(1);
-    var sht = new SHT30(port);
-    await sht.init();
-    while (1) {
-      var data = await sht.readData();
-      document.getElementById("temperature").innerHTML = data.temperature.toFixed(2) + "degree";
-      document.getElementById("humidity").innerHTML = data.humidity.toFixed(2) + "%";
-      await sleep(200);
-    }
-  } catch (error) {
-    console.error("error", error);
+  const temperatureDisplay = document.getElementById("temperatureDisplay");
+  const humidityDisplay = document.getElementById("humidityDisplay");
+  const i2cAccess = await navigator.requestI2CAccess();
+  const port = i2cAccess.ports.get(1);
+  const sht30 = new SHT30(port, 0x44);
+  await sht30.init();
+
+  while (true) {
+    const { humidity, temperature } = await sht30.readData();
+    temperatureDisplay.innerHTML = `${temperature.toFixed(2)} ℃`;
+    humidityDisplay.innerHTML = `${humidity.toFixed(2)} %`;
+    await sleep(500);
   }
 }
-
