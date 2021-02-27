1a44d5f1037e2b57b1443aa798078874694a8c49
gurezo <akihiko.kigure@gmail.com>
2021-02-27 16:33:33 +0900

    dmake, tern, script add

---
+++ gc/polyfill/polyfill.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for polyfill.js
+// Project: [LIBRARY_URL_HERE] 
+// Definitions by: [YOUR_NAME_HERE] <[YOUR_URL_HERE]> 
+// Definitions: https://github.com/borisyankov/DefinitelyTyped
+
+/**
+ * Utility function for async/await code.
+ * @param {number} ms - milliseconds to wait
+ * @return {Promise} A promise to be resolved after ms milliseconds later.
+ * @param ms 
+ * @return  
+ */
+declare function sleep(ms : number): any;
+
+/**
+ * 
+ */
+export declare var session : number;
